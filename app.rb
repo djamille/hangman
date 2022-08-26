b0de9a8a7f05e35b198768c1d505e032dbd5f828
DjamilleVieira <djamille@hotmail.com>
2022-08-26 18:47:05 -0300

    validando se o jogador digitou duas vezes a mesma letra

--- app.rb
+++ app.rb
@@ -11,34 +11,34 @@ tentativas = 0
 erradas = []
 certas = []
 
-until tentativas == 20
-    puts "Você tem #{20 - tentativas} tentativas!"
+until tentativas == 15
+    puts "Você tem #{15 - tentativas} tentativas!"
     puts "Digite uma letra "
     letra = gets.chomp
-
-    tentativas += 1
-
-    if forca.include?("#{letra}") == true
-        certas.push(letra)
-        print "Você acertou! A palavra tem a letra #{letra} \n\n"
+    
+    if certas.include?(letra) || erradas.include?(letra)
+        puts "Você ja digitou essa letra. Tente outra!"
     else
-        erradas.push(letra)
-        print "Ah, que pena! Não tem a letra #{letra}  \n\n"
-    end
+        tentativas += 1
 
-    forca.each do |letra_forca|
-        if certas.include?("#{letra_forca}")
-            print letra_forca
-        else 
-            print "__"
+        if forca.include?("#{letra}") == true
+            certas.push(letra)
+            print "Você acertou! A palavra tem a letra #{letra} \n\n"
+        else
+            erradas.push(letra)
+            print "Ah, que pena! Não tem a letra #{letra}  \n\n"
         end
-    end
+
+        forca.each do |letra_forca|
+            if certas.include?("#{letra_forca}")
+                print letra_forca
+            else 
+                print "__"
+            end
+        end
+    end    
 end
 
 # Avisar quando vencer
-# Não permitir digitar 2x a mesma letra
 # arrumar os espaçamentos
 
-
-#letras_digitadas = []
-#letras_digitadas.push
